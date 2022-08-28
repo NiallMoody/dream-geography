d3d1651a8fa16ce33bc7a7dc260d21d0f71266ff
Glitch (glitch-hello-node) <none>
2022-08-28 16:59:06 +0000

    👨‍👨‍👦🎺 Checkpoint ./src/pages/map.hbs:14049830/1000 ./public/map.js:14049830/78

--- public/map.js
+++ public/map.js
@@ -1,10 +1,12 @@
 var map = null;
+var bounds = null;
+var imageLayer = null;
 
 window.addEventListener('load', (event) => {
-  map = L.map('map', {crs: L.CRS.Simple, minZoom: -5});
+  map = L.map('map', {crs: L.CRS.Simple});
   
-  let bounds = [[0, 0], [1000, 1000]];
-  let image = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map.jpg?v=1661703228321').addTo(map);
+  bounds = [[0, 0], [1000, 1000]];
+  imageLayer = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map.jpg?v=1661703228321').addTo(map);
   
   map.fitBounds(bounds);
 });
