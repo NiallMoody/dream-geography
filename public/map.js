4646f6808d0790c83031acbd7b314726e2d236de
Glitch (glitch-hello-node) <none>
2022-08-28 20:58:02 +0000

    🎉💍 Checkpoint ./public/map.js:14049830/1576

--- public/map.js
+++ public/map.js
@@ -11,22 +11,56 @@ window.addEventListener('load', (event) => {
   map.fitBounds(bounds);
   
   map.on('mousemove', (event) => {
+    let mapElement = document.getElementById('map');
     let foundIndex = -1;
     
-    for(int i=0;i<dreams.length;++i) {
-      if()
+    for(let i=0;i<dreams.length;++i) {
+      if(distance(dreams[i].latlng.lat, dreams[i].latlng.lng, event.latlng.lat, event.latlng.lng) < 8.0) {
+        console.log(`found: ${i}`);
+        foundIndex = i;
+        break;
+      }
+    }
+    
+    if(foundIndex > -1) {
+      mapElement.style.cursor = 'pointer';
+    }
+    else {
+      mapElement.style.cursor = 'grab';
     }
   });
   
   map.on('click', (event) => {
+    
+    
     console.log(event.latlng);
   });
 });
   
 function distance(lat1, lng1, lat2, lng2) {
   let retval = 0.0;
+  let lat = lat2 - lat1;
+  let lng = lng2 - lng1;
   
-  retval = Math.sqrt()
+  retval = Math.sqrt((lat * lat) + (lng * lng));
+  
+  console.log(lat1, lng1, lat2, lng2, retval);
+  console.log(lat, lng);
+  
+  return retval;
+}
+
+//Returns -1 if no dream is under the cursor right now.
+function getIndexUnderCursor(x, y) {
+  let mapElement = document.getElementById('map');
+  let retval = -1;
+    
+  for(let i=0;i<dreams.length;++i) {
+    if(distance(dreams[i].latlng.lat, dreams[i].latlng.lng, event.latlng.lat, event.latlng.lng) < 8.0) {
+      retval = i;
+      break;
+    }
+  }
   
   return retval;
 }
