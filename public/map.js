f57ae0502a25e3a35c93b8b68eb4757586e261ea
Glitch (glitch-hello-node) <none>
2022-08-28 17:35:03 +0000

    😘😎 Checkpoint ./public/map.js:14049830/1066 ./public/style.css:14049830/335 ./src/pages/map.hbs:14049830/102

--- public/map.js
+++ public/map.js
@@ -1,20 +1,10 @@
-var map = null;
-var bounds = [[0, 0], [1000, 1000]];
-var imageLayer = null;
-
 window.addEventListener('load', (event) => {
-  map = L.map('map', {
+  var map = L.map('map', {
     crs: L.CRS.Simple
   });
-  
-  map.on('overlayadd', (event) => {
-    console.log(`overlay added: ${event}`);
-    ///map.fitBounds(bounds);
-  });
-  
-  //bounds = [[0, 0], [1000, 1000]];
-  imageLayer = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map.jpg?v=1661703228321').addTo(map);
-  
-  console.log(`overlay added:`, map);
-  setTimeout(() => {console.log(`timeout`, map); map.fitBounds(bounds);}, 1000);
+
+  var bounds = [[0, 0], [578, 1000]];
+  var image = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map.jpg?v=1661703228321', bounds).addTo(map);
+
+  map.fitBounds(bounds);
 });
