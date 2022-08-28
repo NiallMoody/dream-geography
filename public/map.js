3c2eff33466876287390deedfab944b834f92851
Glitch (glitch-hello-node) <none>
2022-08-28 17:11:06 +0000

    ⛵️🦃 Checkpoint ./public/map.js:14049830/377

--- public/map.js
+++ public/map.js
@@ -1,12 +1,19 @@
 var map = null;
-var bounds = null;
+var bounds = [[0, 0], [1000, 1000]];
 var imageLayer = null;
 
 window.addEventListener('load', (event) => {
-  map = L.map('map', {crs: L.CRS.Simple});
+  map = L.map('map', {
+    crs: L.CRS.Simple
+  });
   
-  bounds = [[0, 0], [1000, 1000]];
+  map.on('overlayadd', (event) => {
+    console.log(`overlay added: ${event}`);
+    ///map.fitBounds(bounds);
+  });
+  
+  //bounds = [[0, 0], [1000, 1000]];
   imageLayer = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map.jpg?v=1661703228321').addTo(map);
   
-  map.fitBounds(bounds);
+  //map.fitBounds(bounds);
 });
