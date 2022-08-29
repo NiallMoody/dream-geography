a879725dc0b3d74ea518acb9c6a14be5b49a3a1e
Glitch (glitch-hello-node) <none>
2022-08-29 08:58:06 +0000

    🌑🎅 Checkpoint ./public/style.css:14049830/43 ./public/map.js:14049830/171 ./src/pages/about.hbs:14049830/394

--- public/map.js
+++ public/map.js
@@ -14,8 +14,14 @@ var dreams = [{url:'places/islands-off-the-coast-of-argyll', latlng:{lat:332.200
 
 window.addEventListener('load', (event) => {
   var map = L.map('map', {
-    crs: L.CRS.Simple
+    crs: L.CRS.Simple,
+    zoomControl: false,
+    attributionControl: false
   });
+  
+  L.control.zoom({
+    zoomOutText: '<span aria-hidden="true">-</span>'
+  }).addTo(map);
 
   var bounds = [[0, 0], [578, 1000]];
   var image = L.imageOverlay('https://cdn.glitch.global/273ac551-9687-45bd-9f8d-1556cfa510c5/map-small.png?v=1661718723063', bounds).addTo(map);
