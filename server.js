652795693a1cf4e531887cba4374fd12459eb1da
Glitch (glitch-hello-node) <none>
2025-06-26 17:09:25 +0100

    Updated about page to point to github for source.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const url = require("url");
 const port = process.env.PORT || 10000;
 
 //------------------------------------------------------------------------------
